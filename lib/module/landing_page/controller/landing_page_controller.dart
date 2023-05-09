7601bd84e55216fe54e76f6145f13c78d9a48e84
Badrulflut <mochbadrullaksamanadin@gmail.com>
2023-05-09 07:08:07 +0700

    update splashscreen

--- lib/module/landing_page/controller/landing_page_controller.dart
+++ lib/module/landing_page/controller/landing_page_controller.dart
@@ -1,14 +1,20 @@
+import 'package:app_progresio/core.dart';
 import 'package:flutter/material.dart';
 import 'package:app_progresio/state_util.dart';
 import '../view/landing_page_view.dart';
 
-class LandingPageController extends State<LandingPageView> implements MvcController {
+class LandingPageController extends State<LandingPageView>
+    implements MvcController {
   static late LandingPageController instance;
   late LandingPageView view;
 
   @override
   void initState() {
     instance = this;
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (_) => const LoginView()));
+    });
     super.initState();
   }
 
